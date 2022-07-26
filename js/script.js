77e278193375e0a16f3df2ce285047c2a45fa6d5
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2022-07-26 11:24:43 +0100

    add php file

--- js/script.js
+++ js/script.js
@@ -20,3 +20,38 @@ for(let i = 0; i < navList.length; i++) {
 const goTop = document.querySelector("#go-to-top");
 
 goTop.addEventListener("click", () => window.scroll(0,0));
+
+
+//contact form
+
+function submitForm(event) {
+    
+    event.preventDefault();
+
+    const form = document.querySelector(".contact-form");
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+
+    const work = document.querySelector("#work");
+    const collab = document.querySelector("#collab");
+    const chat = document.querySelector("#chat");
+    const other = document.querySelector("#other").value;
+
+    const message = document.querySelector("#message").value;
+
+    let subject = "";
+
+    if(work.checked == true) {
+        subject = work.value;
+    } else if (collab.checked == true){
+        subject = collab.value;
+    } else if (chat.checked == true) {
+        subject = chat.value;
+    } else {
+        subject = other;
+    }
+
+    window.open("mailto: adetoyeadewoye@gmail.com?subject=subject&body=body");
+    console.log(name, email, subject, message);
+}
