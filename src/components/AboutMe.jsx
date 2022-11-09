e520b5db4dee3fffbc291782ce420c72c47cd8a2
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2022-11-09 07:55:12 +0100

    Update about me description

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,32 +8,24 @@ const AboutMe = () => {
       </div>
       <div className="about-me-text">
         <p>
-          My name is Adetoye, and I'm a front-end web developer. I specialize in
-          front-end website development utilizing HTML, CSS, and JavaScript. I'm
-          also well-versed with PHP and the React JS framework.
+          I specialize in creating front-end web applications using HTML, CSS,
+          and JavaScript. I'm also well-versed with TailwindCSS and the React JS
+          framework.
         </p>
         <p>
-          I design websites for a variety of purposes, including landing pages,
-          e-commerce storefronts, blogs, portfolios, and commercial websites,
-          and I optimize them for the best possible user experience.
-        </p>
-        <p>
-          Problem solving is my favorite aspect of my job. I enjoy working with
-          businesses and assisting them in determining what type of website is
-          ideal for their needs.
+          I create websites for a variety of purposes, and I always optimize for
+          the best possible user experience.
         </p>
         <p>
           Before switching to a professional career, I started programming as a
-          hobby, creating a variety of fun projects, this makes it very easy for
-          me to solve challenging tasks because I always find an interesting way
-          to break it down into smaller tasks while having fun.
+          hobby, creating a variety of fun projects, this has made it very easy
+          for me to solve challenging tasks because I always find an interesting
+          way to break it down into smaller tasks while having fun. I also enjoy
+          learning and I aspire to work in an environment that allows me learn
+          and continuously improve my skills.
         </p>
         <p>
-          I'm also interested in sales and marketing, and I enjoy advising
-          businesses and startups on how to best increase revenue through their
-          websites. My goal is to assist as many businesses as possible in
-          getting the most out of their websites in terms of generating traffic,
-          increasing revenue, and providing the best user experiences.
+          <span className="interests">Interests: </span>Cosmology, Art & Design
         </p>
       </div>
     </div>
