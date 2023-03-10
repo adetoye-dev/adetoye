e298e66d14c9974a39fc26a97386742fc87715e2
Adetoye Adewoye <71382301+adetoye-dev@users.noreply.github.com>
2023-03-10 21:25:41 +0100

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -8,10 +8,16 @@ const AboutMe = () => {
       </div>
       <div className="about-me-text">
         <p>
-          I am a creative web developer who specializes in creating high-quality front-end applications with a deep understanding of the ReactJS framework. I'm very particular about creating products that offer unparalleled user experiences, as I strive to incorporate creative solutions to challenging tasks.
+          I'm a creative front-end developer focused on creating functional and reusable web components using the ReactJs framework, as well as developing and integrating REST APIs for real-time data fetching and rendering to the user.
         </p>
         <p>
-          With a passion for programming, I started as a hobbyist and have since gained valuable experience in problem-solving and creative coding. I'm passionate about learning, and I'm always striving to stay ahead of the curve by honing my skills and staying up-to-date with the latest technologies.
+          I'm an active team player, and I always do my best to engage the team with my work progress at every stage of development. 
+        </p>
+        <p>
+          The most exciting project I worked on was a component builder app, which I built at Chingu alongside other developers on my team. This web app allows users to drag and drop essential web components to the page and generates ready-to-use HTML code, which enables users with little to no coding knowledge to create simple, static websites. 
+        </p>
+        <p>
+        I'm passionate about open-source projects and contribute to open-source projects in my spare time.
         </p>
       </div>
     </div>
