c2c5d4fba9662b2bc24923cdfa96930fc4577fe0
Adetoye Adewoye <adetoyeadewoye@gmail.com>
2024-05-16 19:05:37 +0100

    setup page router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import Profiles from "./pages/Profiles";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profiles",
+    element: <Profiles />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
